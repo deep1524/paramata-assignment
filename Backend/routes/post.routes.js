c4fc8eff34cfe0d992e5c7f27ab093fa4b007780
Deepkamal yadav <dkits1995@gmail.com>
2023-05-05 14:25:37 +0530

    Assignment Complete

---
+++ Backend/routes/post.routes.js
@@ -0,0 +1,19 @@
+const { verifyToken } = require("../Middleware/authentication.js");
+// const { getFeedPosts, getUserPosts, likePost } = require ("../controllers/posts.js");
+
+const express =require ("express");
+const { getFeedPosts, getUserPosts, likePost } = require("../controllers/posts.js");
+
+
+
+const router = express.Router();
+
+/* READ */
+
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
+
+/* UPDATE */
+router.patch("/:id/like", verifyToken, likePost);
+
+module.exports= router;
